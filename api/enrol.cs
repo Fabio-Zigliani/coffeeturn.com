2ec38e6089a472b0da9b1d9f25a1901ed7673a0a
Fabio Zigliani <fabio.zigliani@gmail.com>
2022-09-30 14:59:39 +1300

    added join page

--- api/enrol.cs
+++ api/enrol.cs
@@ -19,7 +19,7 @@ namespace CoffeeTurn.Identity
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string name = req.Query["number"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
