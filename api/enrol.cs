f999fcf65f920a9377c7ea5224dbd2db23031579
Fabio Zigliani <fabio.zigliani@gmail.com>
2022-10-09 15:58:45 +1300

    fixed compile error

--- api/enrol.cs
+++ api/enrol.cs
@@ -30,22 +30,14 @@ namespace CoffeeTurn.Identity
             string responseMessage = string.IsNullOrEmpty(mobile)
                 ? $"This HTTP triggered function executed successfully. requestbody.mobile -{data?.mobile}- Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello mobile -requestbody.mobile -{data?.mobile}- mobile -{mobile}-. This HTTP triggered function executed successfully.";
-            resultObject = new OkObjectResult(responseMessage);
-            resultObject
 
-            var response = Request.CreateResponse<string>(
-                HttpStatusCode.OK,responseMessage
-            );
-
+            var response = Request.CreateResponse<string>(HttpStatusCode.OK,responseMessage);
             var cookie = new CookieHeaderValue("IEM", "Success");
             cookie.Expires = DateTimeOffset.Now.AddMinutes(60);
             cookie.Domain = Request.RequestUri.Host;
             cookie.Path = "/";
-
             response.Headers.AddCookies(new CookieHeaderValue[] { cookie });
-
             return response;
-
  //           return new OkObjectResult(responseMessage);
         }
     }
